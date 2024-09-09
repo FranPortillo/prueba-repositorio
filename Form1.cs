3a5d9d1798a22dfacf77c54d9ca03205f7602d2e
francisco portillo <@franportillo113@gmail.com>
2024-09-09 00:31:19 -0600

    Se añadio un message box

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace prueba_repositorio
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Esto es una prueba de git");
+        }
     }
 }
